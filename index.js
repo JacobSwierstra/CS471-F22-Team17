1e2199e22f67787c63281b2524f5e07c462d8dd7
hunterireland <hunterireland@u.boisestate.edu>
2022-10-11 16:04:43 -0600

    Closes #17

--- index.js
+++ index.js
@@ -19,26 +19,37 @@ client.once('ready', () => {
     });
 });
 client.once('reconnecting', () => {
-console.log('Reconnecting!');
+    console.log('Reconnecting!'); 
 });
 client.once('disconnect', () => {
-console.log('Disconnect!');
+    console.log('Disconnect!');
 });
 
 // Listen to messages
 client.on('message', async message => {
     // Ignore messages from the bot
     if (message.author.bot) return;
-    //const voiceChannel = message.member.voice.channel;
-    //if (!voiceChannel && (message.content.startsWith(prefix))){
-    //    message.channel.send("You need to be in a voice channel to play music!");
-    //    return;
-    //}
     // Ignore messages that don't start with the prefix
-    if (!message.content.startsWith(prefix))return;
+    if (!message.content.startsWith(prefix)) return;
     
     // Hello world command
     if (message.content.startsWith(`${prefix}test`)) {
         message.channel.send('Hello World!');//.reply if want to @ the user in message
+        return;
+    }
+    // join command
+    if (message.content.startsWith(`${prefix}join`)) {
+        const voiceChannel = message.member.voice.channel;
+        //if (!voiceChannel && (message.content.startsWith(prefix))){
+        //    message.channel.send("You need to be in a voice channel to play music!");
+        //    return;
+        //}
+        try {
+            var connection = await voiceChannel.join();
+            return;
+        } catch (err) {
+            console.log(err);
+            return message.channel.send(err);
+        }
     }
 });
